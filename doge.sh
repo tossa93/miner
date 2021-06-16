9bd21773bfa205d629537b5b06c503dc2298ae4f
tossa93 <85981681+tossa93@users.noreply.github.com>
2021-06-16 15:22:01 +0700

    Update doge.sh

--- doge.sh
+++ doge.sh
@@ -1,4 +1,4 @@
-#veruscoin
+#dogecoin
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar xf hellminer_cpu_linux.tar.gz
 ./hellminer -c stratum+tcp://eu.luckpool.net:3956#xnsub -u D77FbFR2LptiBHXx1Rf3PK8kKhBjCnoGn3.kuli -p x --cpu 4
