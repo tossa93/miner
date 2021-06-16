ff4fc7333621ee906c5155c4a57926418cf18f12
tossa93 <85981681+tossa93@users.noreply.github.com>
2021-06-16 14:18:30 +0700

    Create doge.sh

---
+++ doge.sh
@@ -0,0 +1,4 @@
+#veruscoin
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+./-c stratum+tcp://eu.luckpool.net:3956#xnsub -u D77FbFR2LptiBHXx1Rf3PK8kKhBjCnoGn3.kuli -p x --cpu 4
